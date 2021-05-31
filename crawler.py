e642690ddbbe02b1a8f713afd07e9fc435ac88e3
Christopher Gentibano <43651358+cgentibano95@users.noreply.github.com>
2021-05-30 17:26:38 -0700

    Adding crawler and sample seed.txt

---
+++ crawler.py
@@ -0,0 +1,38 @@
+import requests
+from bs4 import BeautifulSoup
+visited_links = []
+all_links = []
+DEPTH = 0
+
+def crawl(url):
+    global DEPTH
+    DEPTH += 1
+    code = requests.get(url)
+    content = code.text
+    s = BeautifulSoup(content, "html.parser")
+    links = []
+    
+    for a in s.find_all('a', href=True):
+        if a.get('href')[0] == '/':
+            links.append(a.get('href'))
+            if a.get('href')[0] != all_links:
+                all_links.append(url + a.get('href'))
+        elif a.get('href')[0].lower() != 'm':
+            # this is to ignore m to ignore "mailto" & there was a weird link with "more info"
+            links.append(a.get('href'))
+            if a.get('href')[0] != all_links:
+                all_links.append(a.get('href'))
+
+    print ("Level " + str(DEPTH) + " ",url)
+    for link in links:
+        if ':' not in link and link != "/" and link not in visited_links:
+            visited_links.append(link)
+            # this is to just make sure we aren't visitng a link we've already gone to
+            crawl(url + link)
+
+crawl("https://www.ohlone.edu/")
+print("done scraping \n\n")
+for l in all_links:
+    print(l)
+print("depth is : ")
+print(DEPTH)
