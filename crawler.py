faf00a6cf2668c1e5ff7d1202a45239f61933812
Christopher Gentibano <cgent003@ucr.edu>
2021-06-08 16:24:58 -0700

    removing duplicatees

--- crawler.py
+++ crawler.py
@@ -10,26 +10,36 @@ crawls a provided url, grabs all href links and stores into all_links
 '''
 def crawl(url):
     global PGS_CRAWLED
+    PGS_CRAWLED.append(url)
 
     print("crawling: " + url)
-    code = requests.get(url)
+    try:
+        code = requests.get(url)
+    except:
+        print("error at this url: " + url)
+        print("skipping to next url")
+        return
     content = code.text
     s = BeautifulSoup(content, "html.parser")
-    PGS_CRAWLED.append(url)
+    
 
     # now find hrefs
     for a in s.find_all('a', href=True):
         if a.get('href'):
             if a.get('href')[0] == '/':
-                if a.get(url + a.get('href')[1:]) not in all_links and (url + a.get('href')[1:]) not in PGS_CRAWLED:
-                    all_links.append(url + a.get('href')[1:])
-                    LINK_QUEUE.append(url + a.get('href')[1:])
+                if ((url + a.get('href').strip()) not in PGS_CRAWLED) and ((url + a.get('href')[1:].strip()) not in PGS_CRAWLED):
+                    if url[-1] != '/':
+                        all_links.append(url + a.get('href').strip())
+                        LINK_QUEUE.append(url + a.get('href').strip())
+                    else:
+                        all_links.append(url + a.get('href')[1:].strip())
+                        LINK_QUEUE.append(url + a.get('href')[1:].strip())
             elif a.get('href')[0].lower() == "h":
                 # this is to ignore m to ignore "mailto" and menus
                 #links.append(a.get('href'))
-                if a.get('href') not in  all_links and a.get('href') not in PGS_CRAWLED:
-                    all_links.append(a.get('href'))
-                    LINK_QUEUE.append(a.get('href'))
+                if (a.get('href') not in  all_links) and (a.get('href').strip() not in PGS_CRAWLED):
+                    all_links.append(a.get('href').strip())
+                    LINK_QUEUE.append(a.get('href').strip())
                 
 '''
 
@@ -46,7 +56,7 @@ def readWriteFiles(outFile, num_pgs):
     for pg in range(num_pgs):
         print(pg)
         crawl(LINK_QUEUE.pop(0))
-        sleep(2)
+        #sleep(2)
 
     # write to file
     f = open(outFile, "a")
@@ -54,12 +64,16 @@ def readWriteFiles(outFile, num_pgs):
         f.write(l + " \n")
     f.close()
 
+    f = open("pgs_crawled.txt", "a")
+    for l in PGS_CRAWLED:
+        f.write(l + " \n")
+    f.close()
+
 
 if __name__ == "__main__":
     outFile = "results.txt"
-    num_pgs = 30
+    num_pgs = 50
     readWriteFiles(outFile, num_pgs)
-    print("done scraping \n\n")
-    print("pages crawled: " + str(PGS_CRAWLED))
+    print("\n\ndone scraping")
     print("results found at: " + outFile)
 
